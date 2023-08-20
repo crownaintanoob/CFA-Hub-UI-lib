a6c40658de4ab57b01ae2a639cef3866285a8741
CrownKingPown <70343800+crownaintanoob@users.noreply.github.com>
2023-08-20 02:36:55 -0400

    Update serverhopPlsDonate.lua

--- serverhopPlsDonate.lua
+++ serverhopPlsDonate.lua
@@ -34,11 +34,11 @@ local function shop() -- infinite yield serverhop
     for _, v in ipairs(
         game:GetService("HttpService"):JSONDecode(
             game:HttpGetAsync(
-                "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Desc&limit=100"
+                "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
             )
         ).data
     ) do
-        if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+        if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId and v.playing >= 12 then
             x[#x + 1] = v.id
         end
     end
