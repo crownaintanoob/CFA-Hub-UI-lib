b8779fb36f28c4e8d0bbcb9eab80efa9164e12dd
CrownKingPown <70343800+crownaintanoob@users.noreply.github.com>
2023-08-21 16:32:26 -0400

    Update serverhp.lua

--- serverhp.lua
+++ serverhp.lua
@@ -1,18 +1,90 @@
-local PlsDonateId = 8737602449
-local WaitingPlaceId = 14195425876
-local function tp()
+if game.PlaceId ~= 8737602449 then
+    return
+end -- failed attempt at autoexec compatability below, feel free to try and fix it
+
+if not game.IsLoaded then
+    game.Loaded:Wait()
+end
+--wait(.5)
+
+--writefile("MinimumDonation.txt",tostring(minimum))
+
+for i, v in pairs(game:GetService("Players"):GetPlayers()) do
+    repeat
+        wait()
+    until v:FindFirstChild("leaderstats")
+end
+
+local GUIDs = {}
+local maxPlayers = 0
+local pagesToSearch = 100
+function Search()
+    local Http =
+        game:GetService("HttpService"):JSONDecode(
+        game:HttpGet(
+            "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100&cursor="
+        )
+    )
+    for i = 1, pagesToSearch do
+        if Http ~= nil and Http.data ~= nil then
+            for _, v in pairs(Http.data) do
+                if v.playing ~= v.maxPlayers and v.id ~= game.JobId then
+                    maxPlayers = v.maxPlayers
+                    table.insert(GUIDs, {id = v.id, users = v.playing})
+                end
+            end
+        end
+        if Http.nextPageCursor ~= null then
+            Http =
+                game:GetService("HttpService"):JSONDecode(
+                game:HttpGet(
+                    "https://games.roblox.com/v1/games/" ..
+                        game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100&cursor=" .. Http.nextPageCursor
+                )
+            )
+        else
+            break
+        end
+    end
+end
+
+local highest = {id = "", users = 0}
+function findHighest()
     local suc =
         pcall(
         function()
-            game:GetService("TeleportService"):Teleport(
-                if game.PlaceId == PlsDonateId then WaitingPlaceId elseif game.PlaceId == WaitingPlaceId then PlsDonateId else PlsDonateId,
-                    game:GetService("Players").LocalPlayer
+            for i, v in ipairs(GUIDs) do
+                if v.users > highest.users and not (v.users > (maxPlayers - 4)) then
+                    highest = v
+                end
+            end
+        end
+    )
+
+    if not suc then
+        Search()
+        findHighest()
+    end
+end
+
+function tp()
+    local suc =
+        pcall(
+        function()
+            game:GetService("TeleportService"):TeleportToPlaceInstance(
+                game.PlaceId,
+                highest.id,
+                game.Players.LocalPlayer
             )
         end
     )
     if not suc then
+        Search()
+        findHighest()
         tp()
     end
 end
 
+Search()
+findHighest()
 tp()
